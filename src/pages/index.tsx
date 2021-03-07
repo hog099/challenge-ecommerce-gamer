f9618c7c2cbb68f6039f87654857a3e025c0057d
Hudson de Oliveira Ginelli <oliveiraginelli@gmail.com>
2021-03-07 20:20:37 -0300

    ajuste next config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,13 @@
 import React from 'react'
-import Head from 'next/head'
-import { GetServerSideProps } from 'next'
-import { ThemeContext } from '../contexts/ThemeContext'
 import styles from '../styles/pages/Home.module.css'
 
 import Home from './home/index';
 
-interface AppProps {
-  theme: 'light-mode' | 'dark-mode';
-  checked: boolean;
-}
-
-export default function App(props: AppProps) {
-  const { theme } = React.useContext(ThemeContext)
-  
+export default function App() {
 
   return (
     <div className={styles.lightMode}>
      <Home />
     </div>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { theme, checked } = ctx.req.cookies
-  return {
-    props: {
-      theme: String(theme),
-      checked: checked === 'true',
-    }
-  }
 }
